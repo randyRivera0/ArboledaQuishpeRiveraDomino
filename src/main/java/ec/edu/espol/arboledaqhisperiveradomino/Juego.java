704d44a4b17b6c26c442c21c17589e7a1ec44905
spupi <rariberm@espol.edu.ec>
2023-12-03 19:16:51 -0500

    Version Final Perfecta

--- src/main/java/ec/edu/espol/arboledaqhisperiveradomino/Juego.java
+++ src/main/java/ec/edu/espol/arboledaqhisperiveradomino/Juego.java
@@ -121,37 +121,7 @@ public class Juego {
         
         ganador(jugador1, jugador2);
     }
-    
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        sc.useDelimiter("\n");
-        
-        System.out.print("Escriba el numero de fichas iniciales: ");
-        nFichasIniciales = sc.nextInt();
-        Juego juego = new Juego();
-        
-        System.out.print("1 Jugador o 2 Jugadores (Escriba 1 o 2): ");
-        String modo = sc.next();
-        
-        while(!(modo.equals("1") || modo.equals("2"))){
-            System.out.print("Opcion no valida. 1 Jugador o 2 Jugadores (Escriba 1 o 2): ");
-            modo = sc.next();
-        }
-        
-        if(modo.equals("1")){
-            modo1(juego, sc);
-            }
-        
-        else if (modo.equals("2")){
-            modo2(juego, sc);
-        }
-                
-    }
-    
-   
-    
-    
 /*
     public void jugarMach(Jugador mach){
         System.out.println("Juega máquina: " + mach.getNombre());
